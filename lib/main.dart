f0bccc4713d808758af8f5f1b7614f24301e437c
Tutu12344 <padde.cross@gmail.com>
2024-03-20 20:31:59 +0900

    firestore function complete

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -71,14 +72,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //Googleアカウントの表示名
-  String _displayName = "";
-
-  static final googleLogin = GoogleSignIn(scopes: [
-    'email',
-    'https://www.googleapis.com/auth/contacts.readonly',
-  ]);
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -92,42 +85,20 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
-              "Hello $_displayName",
-              style: const TextStyle(fontSize: 50),
+          child: TextButton(
+            //ボタンを押したときのイベント
+            onPressed: () {
+              //データの取得
+              FirebaseFirestore.instance
+                  .collection('flutterDataCollection')
+                  .doc("flutterDataDocument")
+                  .delete();
+            },
+            child: const Text(
+              "実行",
+              style: TextStyle(fontSize: 50),
             ),
-            TextButton(
-              //ボタンを押したときのイベント
-              onPressed: () async {
-                //Google認証
-                GoogleSignInAccount? signinAccount = await googleLogin.signIn();
-                if (signinAccount == null) return;
-                GoogleSignInAuthentication auth =
-                    await signinAccount.authentication;
-                final OAuthCredential credential =
-                    GoogleAuthProvider.credential(
-                  idToken: auth.idToken,
-                  accessToken: auth.accessToken,
-                );
-                //認証情報をFirebaseに登録
-                User? user = (await FirebaseAuth.instance
-                        .signInWithCredential(credential))
-                    .user;
-                if (user != null) {
-                  setState(() {
-                    //画面を更新
-                    _displayName = user.displayName!;
-                    print(user.displayName);
-                  });
-                }
-              },
-              child: const Text(
-                "login",
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          ]),
+          ),
         ));
   }
 }
